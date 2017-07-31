7d76157f98c1f1e9227dfc2416b8f4d34fac36c8
Jessica Sheridan and Kate Mashokha <jessica@>
2017-07-31 16:02:58 -0700

    files for testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,22 @@
+import models.PingPong;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("I'm a ping-pong application");
+        System.out.println("Please enter a number to count up to: ");
+
+        try {
+            int userNumber = Integer.parseInt(reader.readLine());
+            PingPong game = new PingPong();
+            System.out.println(game.runPingPong(userNumber));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/test/java/models/PingPongTest.java
@@ -0,0 +1,78 @@
+package models;
+import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 7/31/17.
+ */
+public class PingPongTest {
+
+    @Test
+    public void runPingPong_countUpToOne_ArrayList(){
+    PingPong testPingPong = new PingPong();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    assertEquals(expectedOutput, testPingPong.runPingPong(1));
+    }
+
+    @Test
+    public void runPingPong_countUpToTwo_ArrayList(){
+        PingPong testPingPong = new PingPong();
+        List<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        assertEquals(expectedOutput, testPingPong.runPingPong(2));
+    }
+
+    @Test
+    public void runPingPong_replaceThreeWithPing() throws Exception {
+        PingPong testPingPong = new PingPong();
+        List<Object> expectedOutput = new ArrayList<>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        assertEquals(expectedOutput, testPingPong.runPingPong(3));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf5_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        List<Object> expectedOutput = new ArrayList<>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(5));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOfBoth3And5_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        List<Object> expectedOutput = new ArrayList<>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        expectedOutput.add("ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("ping");
+        expectedOutput.add("pong");
+        expectedOutput.add(11);
+        expectedOutput.add("ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("pingpong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(15));
+    }
+
+
+
+}
